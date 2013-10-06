02c0091d78870b7d0251de132f68a2cf1f52f04e
smith11235 <smith11235@gmail.com>
2013-10-06 19:51:48 -0400

    added crafty.js, added app/assets/game/*.*, added game controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rpg::Application.routes.draw do
+  get "game/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
